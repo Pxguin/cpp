8afd6ce4feadcab227762b2a4cb0f392157b6006
Pxguin <95258627+Pxguin@users.noreply.github.com>
2024-02-18 18:15:30 -0800

    d

--- usaco/silver/2017/feb/cow_cross_ii.cpp
+++ usaco/silver/2017/feb/cow_cross_ii.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// new code (lost the original)
+
 int main() {
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	freopen("maxcross.in","r",stdin); freopen("maxcross.out","w",stdout);
